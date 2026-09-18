fc03f23b4c4982818616d0474cd2337ebdcaa201
koodiOltsi <olli.loyttyniemi@kolumbus.fi>
2026-09-18 19:49:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,119 @@
+require('dotenv').config()
+const express = require('express')
+const Person = require('./models/person')
+
+const app = express()
+
+var morgan = require('morgan')
+morgan.token('body', request => JSON.stringify(request.body))
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(morgan(':method :url :status :response-time ms :body'))
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
+})
+
+app.get('/info', (request, response) => {
+  Person.countDocuments().then((count) => {
+    response.send(`Phonebook has info for ${count} people <br><br>
+     ${new Date()} `)
+  })
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing',
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing',
+    })
+  }
+  
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then((savedPerson) => {
+    response.json(savedPerson)
+    app.use(morgan(':method :url :status :response-time ms :body'))
+  })
+  .catch((error) => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch((error) => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
+  })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ mongo.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://olliloyttyniemi_db_user:${password}@cluster0.9avnjgk.mongodb.net/personApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+// Add this BEFORE your MongoDB connection code
+// Ilman tätä, Node.js saattaa käyttää IPv6-osoitteita, mikä voi aiheuttaa ongelmia joidenkin MongoDB-palvelimien kanssa. Tämä asetus pakottaa Node.js:n käyttämään IPv4-osoitteita.
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']); // Google DNS
+
+//mongoose.connect(url)
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+/*
+ const person = new Person({
+   name: 'Aku Ankka',
+   number: '03-0303030',
+ })
+*/
+
+if (process.argv.length < 4) {
+  console.log("Phonebook:" )
+  Person
+ .find({}).then((persons) => {
+  persons.forEach((person) => {
+    console.log(person.name, person.number)
+  })
+  mongoose.connection.close()
+})
+}
+
+if (process.argv.length > 4) {
+    const person = new Person({
+      name: process.argv[3],
+      number: process.argv[4],
+    })
+
+    person.save().then((result) => {
+      console.log('added', person.name, 'number', person.number, 'to phonebook')
+      mongoose.connection.close()
+    })
+}
+
+ 
+
